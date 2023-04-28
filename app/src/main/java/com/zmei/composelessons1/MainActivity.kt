09b6fe1073f31a0bc0279af78d1dab092443c98b
arhizmei <arhizmei@gmail.com>
2023-04-28 11:21:34 +0300

    work2

--- app/src/main/java/com/zmei/composelessons1/MainActivity.kt
+++ app/src/main/java/com/zmei/composelessons1/MainActivity.kt
@@ -3,25 +3,42 @@ package com.zmei.composelessons1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.tooling.preview.Preview
 import com.zmei.composelessons1.ui.theme.ComposeLessons1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            NewText()
-        }
-    }
-    @Preview (showBackground = true)
-    @Composable
-    fun NewText(){
-        Text(text = "Hello world!")
-    }
+            Row(modifier = Modifier
+                .background(Color.Gray)
+                .fillMaxSize(),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+
+            Column(
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.End){
+                Text(text = "Hello world!")
+                Text(text = "Second line")
+                Text(text = "Third line") }
+        Column(
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.End){
+            Text(text = "Hello world!")
+            Text(text = "Second line")
+            Text(text = "Third line") }
+        }}}
+
+
 }
